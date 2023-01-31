4ac28e37a8dd77c2e9f1e97a53431e36fcac5375
Alhafat <vladimir-lukin@inbox.ru>
2023-01-31 20:21:20 +0300

    task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -4,3 +4,30 @@
 
 // [-4, -6, 89, 6] -> 0
 
+Console.Clear();
+
+int[] GetArray ()
+{
+    System.Console.WriteLine("Введите количество значений массива:");
+    int size=int.Parse(Console.ReadLine()!);
+    int[] array=new int[size];
+    for (int i = 0; i <size; i++)
+    {
+        array[i]=new Random().Next(100,1000);
+    }
+    return array;
+}
+
+int findNumbers(int[] numbers, int summ)
+{
+    for (int i =1; i <numbers.Length; i++)    
+    {
+        summ+=numbers[i];
+    }
+    return summ;
+}
+
+int[] numbers=GetArray();
+System.Console.WriteLine(string.Join(", ", numbers));
+int result=findNumbers(numbers, summ);
+System.Console.WriteLine(string.Join(", ", result));
